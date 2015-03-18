3a86e1a146922f3b24e50d25880cbc25f5cae047
Levent Ali <lebreeze@gmail.com>
2015-03-18 22:01:08 +0000

    Works with magic and glitter

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'github-pages'
+gem 'compass', '~> 1.0.1'
--- Gruntfile.js
+++ Gruntfile.js
@@ -22,32 +22,78 @@ module.exports = function(grunt) {
       }
     },
 
+    compass: {
+      options: {
+        // If you're using global Sass gems, require them here.
+        // require: ['singularity', 'jacket'],
+        sassDir: 'app/_sass',
+        cssDir: '.tmp/css',
+        imagesDir: 'app/img',
+        javascriptsDir: 'app/js',
+        relativeAssets: false,
+        httpImagesPath: '/img',
+        httpGeneratedImagesPath: '/img/generated',
+        outputStyle: 'expanded',
+        raw: 'extensions_dir = "app/_bower_components"\n'
+      },
+      dist: {
+        options: {
+          generatedImagesDir: '<%= yeoman.dist %>/img/generated'
+        }
+      },
+      server: {
+        options: {
+          debugInfo: true,
+          generatedImagesDir: '.tmp/img/generated'
+        }
+      }
+    },
+
     sass: { // sass tasks
       dist: {
         options: {
-          compass: false, // enable the combass lib, more on this later
+          compass: true, // enable the combass lib, more on this later
           style: 'expanded' // we don't want to compress it
         },
         files: {
-          'css/til.css': '_sass/main.scss' // this is our main scss file
+          'css/main.css': '_sass/main.scss' // this is our main scss file
         }
       }
     },
 
     cssmin: { // minifying css task
       dist: {
-        files: {
-          'css/til.min.css': 'css/til.css'
+        options: {
+          check: 'gzip'
         }
       }
     },
 
 
+
+    copy: {
+      // Copy CSS into .tmp directory for Autoprefixer processing
+      stageCss: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'app/css',
+          src: '**/*.css',
+          dest: '.tmp/css'
+        }]
+      }
+    },
+
+
     watch: { // watch task for general work
-      sass: {
-        files: ['app/_sass/**/*.scss'],
-        tasks: ['sass']
+      compass: {
+        files: ['app/_sass/**/*.{scss,sass}'],
+        tasks: ['compass:server']
       },
+      // sass: {
+      //   files: ['app/_sass/**/*.scss'],
+      //   tasks: ['sass']
+      // },
       styles: {
         files: ['css/til.css'],
         tasks: ['cssmin']
@@ -57,17 +103,6 @@ module.exports = function(grunt) {
         tasks: ['jekyll:server']
       }
 
-    },
-    concurrent: {
-      server: [
-        // 'compass:server',
-        // 'copy:stageCss',
-        'jekyll:server'
-      ],
-      dist: [
-        // 'compass:dist',
-        // 'copy:dist'
-      ]
     },
 
     browserSync: {
@@ -90,14 +125,28 @@ module.exports = function(grunt) {
           watchTask: true
         }
       },
-    }
+    },
+
+    concurrent: {
+      server: [
+        'compass:server',
+        'copy:stageCss',
+        'jekyll:server'
+      ],
+      dist: [
+        // 'compass:dist',
+        // 'copy:dist'
+      ]
+    },
 
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-jekyll');
   grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-concurrent');
