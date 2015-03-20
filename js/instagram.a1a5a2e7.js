b4827495e0af07f7cd1e728f112d4a3872b012b5
Levent Ali <lebreeze@gmail.com>
2015-03-20 16:30:54 +0000

    Built things-i-like-in from commit b41eac8 on branch master

--- js/instagram.a1a5a2e7.js
+++ js/instagram.a1a5a2e7.js
@@ -2,6 +2,7 @@ if(!!document.getElementById('instafeed')){
   var feed = new Instafeed({
     get: 'user',
     userId: 1049440,
+    limit: 30,
     accessToken: '1049440.467ede5.9359d166205645c48e4eec075239cb66'
   });
   feed.run();
